ebd2c5f0d70bfb941f06c082da2719b6c5f7a407
Mario Clemente Ramírez <marioclementramirez@gmail.com>
2024-04-30 11:57:54 +0200

    Updated the .jar in the example folder
    Along with a few minor changes to the scripts and the pom.

--- example/dir_auto_linux.sh
+++ example/dir_auto_linux.sh
@@ -1,2 +1,2 @@
-java -jar jar\dir_auto_linux.jar test_resources\testArchive.zip test_resources\test.csv
+java -jar jar\dir_auto.jar test_resources\testArchive.zip test_resources\test.csv
 read -n1 -r -p "Press any key to continue..." key
